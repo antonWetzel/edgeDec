fb56952660bd72a915bd6e2c8a6418c3501fc883
antonWetzel <59712243+antonWetzel@users.noreply.github.com>
2021-12-04 15:16:03 +0100

    Update graph.js

--- out/visualize/graph/graph.js
+++ out/visualize/graph/graph.js
@@ -132,7 +132,7 @@ export class Box {
         this.x = field.clientWidth / 2;
         this.y = field.clientHeight / 2;
         this.body.onmousedown = (ev) => {
-            if (ev.ctrlKey) {
+            if (ev.ctrlKey || ev.metaKey) {
                 dragged = new Line(this);
                 modus = "line";
             }
